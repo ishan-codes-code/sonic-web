5cd9ba871371dbb89e891c7e67c010318ade3c5d
Ishan Srivastava <dev.srivastava.ishan@gmail.com>
2026-05-23 23:32:19 +0000

    typography fix, assets addition

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import animations from "./lib/tailwind.animations";
+
+const config = {
+  theme: {
+      extend: {
+            ...animations,
+                },
+                  },
+                  };
